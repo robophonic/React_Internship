abbbed9b2b565494c637ca9abfde42c066c30f3f
unknown <robophonic02@gmail.com>
2021-02-11 13:08:53 +0200

    1 урок 1.1

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,36 +10,36 @@ class Footer extends Component {
                         <div className="footer_menu">
                             <ul className="nav">
                                 <li className="has_dropdown">
-                                    <a href="\#" className="inactive">Problem & Challenges</a>
+                                    <a href="\#" rel="noreferrer" className="inactive">Problem & Challenges</a>
                                     <ul>
-                                        <li><a href="\#">History</a></li>
-                                        <li><a href="\#">Problem industry</a></li>
-                                        <li><a href="\#">Challenge</a></li>
+                                        <li><a href="\#" rel="noreferrer">History</a></li>
+                                        <li><a href="\#" rel="noreferrer">Problem industry</a></li>
+                                        <li><a href="\#" rel="noreferrer">Challenge</a></li>
                                     </ul>
                                 </li>
                                 <li className="has_dropdown">
-                                    <a href="\#" className="inactive">Platform & Foundation </a>
+                                    <a href="\#" rel="noreferrer" className="inactive">Platform & Foundation </a>
                                     <ul>
-                                        <li><a href="\#">Our Vision</a></li>
-                                        <li><a href="\#">What Makes Us Different</a></li>
+                                        <li><a href="\#" rel="noreferrer">Our Vision</a></li>
+                                        <li><a href="\#" rel="noreferrer">What Makes Us Different</a></li>
                                     </ul>
                                 </li>
                                 <li className="has_dropdown">
-                                    <a href="\#" className="inactive">Token</a>
+                                    <a href="\#" rel="noreferrer" className="inactive">Token</a>
                                     <ul>
-                                        <li><a href="\#">UOP Token</a></li>
-                                        <li><a href="\#">Token Economics</a></li>
+                                        <li><a href="\#" rel="noreferrer">UOP Token</a></li>
+                                        <li><a href="\#" rel="noreferrer">Token Economics</a></li>
                                     </ul>
                                 </li>
                                 <li className="has_dropdown">
-                                    <a href="\#">OUR OPERATIONS</a>
+                                    <a href="\#" rel="noreferrer">OUR OPERATIONS</a>
                                 </li>
                             </ul>
                         </div>
                         <div className="footer_info order-first">
                             <p className="copyright">Федоров Владислав</p>
-                            <a href="https://github.com/robophonic" target="_blank" className="terms-link">ссылка на github аккаунт</a>
-                            <a href="https://www.figma.com/file/biFld9Yt7dpWURZ2ul3dE0/Utopia-foundation-web-site?node-id=456%3A1721" target="_blank" className="terms-link">ссылка на оригинал дизайна</a>
+                            <a href="https://github.com/robophonic" rel="noreferrer" target="_blank" className="terms-link">ссылка на github аккаунт</a>
+                            <a href="https://www.figma.com/file/biFld9Yt7dpWURZ2ul3dE0/Utopia-foundation-web-site?node-id=456%3A1721" rel="noreferrer" target="_blank" className="terms-link">ссылка на оригинал дизайна</a>
                         </div>
                     </div>
                 </div>
